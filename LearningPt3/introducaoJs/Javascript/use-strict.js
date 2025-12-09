0780b2b0be9fea702415b63e123c16fdeba0704b
Foqsz <contatovictorvinicius05@gmail.com>
2025-12-09 10:55:27 -0300

    feat: use strict

---
+++ LearningPt3/introducaoJs/Javascript/use-strict.js
@@ -0,0 +1,8 @@
+// exemplo1.js  
+"use strict"; // Modo estrito do JavaScript, evita alguns erros comuns, como criar variáveis globais acidentalmente.    
+
+function bet(){
+    x = 10; 
+}
+bet();
+console.log(x); 
