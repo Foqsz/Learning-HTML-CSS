b5e03ab3f931b274aeaaafb1468bfbf0e0662fab
Foqsz <contatovictorvinicius05@gmail.com>
2025-12-17 11:48:13 -0300

    feat: calcular imc

---
+++ LearningPt3/introducaoJs/Javascript/desafio2/calcularImc.js
@@ -0,0 +1,37 @@
+function calcularImc(peso, altura) {
+  if (isNaN(peso) || isNaN(altura)) {
+    throw new Error(
+      "Por favor, insira valores numéricos válidos para peso e altura."
+    );
+  }
+
+  if (peso <= 0 || altura <= 0) {
+    throw new Error("Peso e altura devem ser maiores que zero.");
+  }
+
+  const Imc = Number(peso) / (Number(altura) * Number(altura));
+  const resultado = document.getElementById("result");
+  resultado.innerHTML = "Seu IMC é: " + Imc.toFixed(2);
+  classificarImc(Imc);
+  return Imc;
+}
+
+function classificarImc(imc) {
+  const el = document.getElementById("classification");
+
+  if (imc >= 16 && imc <= 16.9) {
+    el.innerHTML = "Classificação: Muito abaixo do peso";
+  } else if (imc <= 18.4) {
+    el.innerHTML = "Classificação: Abaixo do peso";
+  } else if (imc <= 24.9) {
+    el.innerHTML = "Classificação: Peso normal";
+  } else if (imc <= 29.9) {
+    el.innerHTML = "Classificação: Acima do peso";
+  } else if (imc <= 34.9) {
+    el.innerHTML = "Classificação: Obesidade grau I";
+  } else if (imc <= 40) {
+    el.innerHTML = "Classificação: Obesidade grau II";
+  } else {
+    el.innerHTML = "Classificação: Obesidade grau III";
+  }
+}   
