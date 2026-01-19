0be9811d18078dc87ed9bc802ced5e6ede57c8f1
Foqsz <contatovictorvinicius05@gmail.com>
2026-01-19 10:20:41 -0300

    feat: add examples for toString and toLocaleString methods

--- Learning-Javascript/introducaoJs/Numbers/introducao.js
+++ Learning-Javascript/introducaoJs/Numbers/introducao.js
@@ -10,3 +10,14 @@ console.log((123).toPrecision(6));
 
 num = 12.231231
 console.log(num.toExponential());
+
+console.log(typeof num.toString())
+
+console.log(num.toString(2))
+
+num = 112212.231231
+console.log(num.toLocaleString());
+console.log(num.toLocaleString("en-EN"));
+console.log(num.toLocaleString("pt-BR", {style: "currency", currency: "BRL"}));
+console.log(num.toLocaleString("pt-PT", {style: "currency", currency: "EUR"}));
+console.log(num.toLocaleString("en-US", {style: "currency", currency: "USD"}));
