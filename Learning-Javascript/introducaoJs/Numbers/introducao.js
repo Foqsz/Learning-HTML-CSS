96c72701914271c27a66d26c49242af3f58857b6
Foqsz <contatovictorvinicius05@gmail.com>
2026-01-19 09:44:03 -0300

    feat: add number formatting examples using toFixed, toPrecision and toExponential

---
+++ Learning-Javascript/introducaoJs/Numbers/introducao.js
@@ -0,0 +1,12 @@
+let num = 1234567.890;
+
+console.log(num.toFixed(2));
+console.log(typeof num.toFixed(2));
+
+console.log(num.toPrecision(5));
+console.log(typeof num.toPrecision(5));
+
+console.log((123).toPrecision(6));
+
+num = 12.231231
+console.log(num.toExponential());
