956e9473d427b50650658142c62c01b193e52f58
Foqsz <contatovictorvinicius05@gmail.com>
2026-01-26 09:22:25 -0300

    feat: add JavaScript file for date manipulation and logging functionality

---
+++ Learning-Javascript/introducaoJs/Date/introducao.js
@@ -0,0 +1,15 @@
+const data = new Date();
+
+console.log(data)
+console.log(data.getDay())
+console.log(data.getDate())
+console.log(data.getMonth())
+console.log(data.getFullYear())
+console.log(data.getHours())
+console.log(data.getMinutes())
+console.log(data.getSeconds())
+console.log(data.getMilliseconds())
+console.log(data.getTime())
+
+console.log(Date.now())
+console.log(Date.parse('2024-06-15T12:00:00'))
