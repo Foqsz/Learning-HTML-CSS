a4b915efe3ff7c81a72f26db7658f7e84dd5cd9d
Foqsz <contatovictorvinicius05@gmail.com>
2026-02-02 11:03:22 -0300

    feat: add String.prototype.replaceAll method for string replacement functionality

---
+++ Learning-Javascript/introducaoJs/POO/replaceAll.js
@@ -0,0 +1,13 @@
+if(!String.prototype.replaceAll)
+{
+        String.prototype.replaceAll = function(busca, troca){
+            if(!(busca instanceof String || typeof busca === "string")){
+                throw new Error("first parameter must be a string")
+            }
+
+            if(!(troca instanceof String || typeof troca === "string")){
+                throw new Error("second parameter must be a string")
+            }
+            return this.valueOf().split(busca).join(troca);
+    }
+} 
